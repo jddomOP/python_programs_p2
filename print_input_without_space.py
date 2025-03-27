e6bd920a1f652853603e6242b66056c6c8d2e62a
Jaydie <johndenieldomalaon@gmail.com>
2025-03-28 02:13:52 +0800

    added another comment, will provide proof if needed

--- print_input_without_space.py
+++ print_input_without_space.py
@@ -6,3 +6,4 @@ print("Output: ", without_space)
 
 #As to why i am coding this late, i feel sick after we did our pathfit activity
 #was tired and sick, got a sinus infection and i felt drained
+#anyways, i already started the batch of code last saturday
